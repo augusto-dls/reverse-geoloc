29cd89ad79cf3f455a09fd05862d17dfcd930400
Augusto de los Santos <augusto_dls@outlook.com>
2026-06-06 15:00:53 -0300

    Update server.py
    funny word

--- server.py
+++ server.py
@@ -169,7 +169,7 @@ def location():
         return jsonify({"error": str(e)}), 502
 
     try:
-        audio_bytes, mime_type = text_to_wav(f"Ubicación: {address}")
+        audio_bytes, mime_type = text_to_wav(f"Estás en {address}, mi nigga")
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
